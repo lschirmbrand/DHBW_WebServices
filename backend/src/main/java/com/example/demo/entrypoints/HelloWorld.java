e450a0f700fe25b586dc93f7f04b8add35d63e87
Niel Rohling <niel.rohling@gmail.com>
2021-02-08 15:01:04 +0100

    simple hello world Controller

---
+++ backend/src/main/java/com/example/demo/entrypoints/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.demo.entrypoints;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/helloWorld")
+public class HelloWorld {
+
+    @GetMapping()
+    public String getHelloWorld() {
+        return "Hello World!";
+    }
+}
