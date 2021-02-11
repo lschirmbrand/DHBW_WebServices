b99d8d7cb580b48966976cce8b20228f4860806b
lschirmbrand <luca.schirmbrand@hotmail.com>
2021-02-11 11:48:37 +0100

    Merge Push

--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -4,11 +4,7 @@ class Title extends React.Component{
     render(){
         return(
             <div className={"Title"}>
-<<<<<<< HEAD
-                <h1>Song-Guesser</h1>
-=======
-                Title Placeholder
->>>>>>> 934c99715e4f66aa95fcbbcf342447185aa66648
+                Song-Guesser
             </div>
         )
     }
