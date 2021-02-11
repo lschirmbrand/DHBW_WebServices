8bf52b2e487cea83fc349301ace3488ebf823969
lschirmbrand <luca.schirmbrand@hotmail.com>
2021-02-11 10:08:20 +0100

    Integrated Timer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import './styles/App.css';
 import "./styles/AnswerBoxElement.css"
 import React from "react"
-
 import AnswerBox from "./components/AnswerBox"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
+import Timer from "./components/Timer"
 
 class App extends React.Component{
   render(){
@@ -14,6 +14,7 @@ class App extends React.Component{
         <Header />
         <Navbar />
         <h1>Hier steht eine Überschrift</h1>
+        <Timer />
         <AnswerBox />
         <Footer />
       </div>     
---
+++ frontend/src/components/Timer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Countdown from 'react-countdown';
+
+class Timer extends React.Component {
+
+    Completionist = <span>You are good to go!</span>
+
+    render(){
+        return(
+          <div>
+              <Countdown date={Date.now() + this.getThemeLength()}>
+                <span>Time is over!</span>
+              </Countdown>
+          </div>
+        )
+    }
+
+    getThemeLength(){
+        return(
+            //Going to return the Length of the Song
+            5000
+        )
+    }  
+}
+export default Timer
