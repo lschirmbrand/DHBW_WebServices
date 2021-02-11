9745ae520d07e763d833595a0613f11fd7f0be6d
FabMaster2000 <68748739+FabMaster2000@users.noreply.github.com>
2021-02-11 11:38:58 +0100

    Titel geändert :)

--- frontend/src/components/Title.js
+++ frontend/src/components/Title.js
@@ -4,7 +4,7 @@ class Title extends React.Component{
     render(){
         return(
             <div className={"Title"}>
-                <h1>Title Placeholder</h1>
+                <h1>Song-Guesser</h1>
             </div>
         )
     }
