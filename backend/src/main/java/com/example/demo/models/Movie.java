419bc4c1d09788b7d38c446af4fa12b3b9021879
Niel Rohling <niel.rohling@gmail.com>
2021-02-12 10:39:24 +0100

    added movies

--- backend/src/main/java/com/example/demo/models/Movie.java
+++ backend/src/main/java/com/example/demo/models/Movie.java
@@ -1,27 +1,41 @@
 package com.example.demo.models;
 
-public class Movie {
-    private String title;
-    private String posterURL;
+import java.util.List;
 
-    public Movie(String title, String posterURL) {
+public class Movie {
+    private final String posterURLPrefix = "https://image.tmdb.org/t/p/w1280";
+
+    private final int id;
+    private final String title;
+    private final String posterURL;
+    private final List<String> genres;
+    private final String overview;
+
+    public Movie(int id, String title, String posterPath, List<String> genres, String overview) {
+        this.id = id;
         this.title = title;
-        this.posterURL = posterURL;
+        this.posterURL = posterURLPrefix + posterPath;
+        this.genres = genres;
+        this.overview = overview;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getPosterURL() {
         return posterURL;
     }
 
-    public void setPosterURL(String posterURL) {
-        this.posterURL = posterURL;
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    public String getOverview() {
+        return overview;
     }
 }
