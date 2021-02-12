1dfb56777607c68d1e955513f88247af5636bce5
Niel Rohling <niel.rohling@gmail.com>
2021-02-12 12:16:01 +0100

    added cors Configuration

--- backend/src/main/java/com/example/demo/Configuration.java
+++ backend/src/main/java/com/example/demo/Configuration.java
@@ -3,9 +3,22 @@ package com.example.demo;
 import com.example.demo.dataproviders.MockMovieProvider;
 import com.example.demo.dataproviders.MovieProvider;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 
     @Bean
     MovieProvider movieProvider() {
