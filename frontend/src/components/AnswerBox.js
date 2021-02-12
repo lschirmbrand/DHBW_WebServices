e63c482db2c1e49ba18ee6c66ab913879f4df63f
FabMaster2000 <68748739+FabMaster2000@users.noreply.github.com>
2021-02-12 15:30:38 +0100

    key and object

--- frontend/src/components/AnswerBox.js
+++ frontend/src/components/AnswerBox.js
@@ -19,7 +19,7 @@ class AnswerBox extends React.Component{
 
     render(){   
         
-        const listItems = this.state.movieData.map((d) => <AnswerElement object={d} name={d.title} thumbnail={d.posterURL}/>)
+        const listItems = this.state.movieData.map((d) => <AnswerElement object={d} key={d.id}/>)
         
         let elem = [{"name":"Star Wars"},{"name":"Herr der Ringe"},{"name":"Harry Potter"}]
         let myArray = []
--- frontend/src/components/AnswerElement.js
+++ frontend/src/components/AnswerElement.js
@@ -4,18 +4,20 @@ class AnswerElement extends React.Component{
     constructor(){
         super()
         this.state ={
-            thumbnailPre: "https://image.tmdb.org/t/p/w1280"
+            
         }
     }
+
+
     render(){
         console.log(this.props.thumbnail)
         return (
             // <div className="AnswerElement">
             <div className={"ElementCell"}>
                 <button onClick={() => {this.checkAnswer(this.props.key)} } className="AnswerElement">
-                    <img className="AnswerElement" src={this.props.thumbnail}></img>
+                    <img className="AnswerElement" src={this.props.object.posterURL}></img>
                 </button>
-                <p className="ElementPar">{this.props.name}</p>
+                <p className="ElementPar">{this.props.object.title}</p>
             </div>
         )        
     }
