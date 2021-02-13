16431de44d83b6c2746e54f719f9eeca5fcbd6c1
lschirmbrand <luca.schirmbrand@hotmail.com>
2021-02-13 11:36:03 +0100

    Timer gefixt, sieht schnieke aus

--- frontend/src/components/Timer.js
+++ frontend/src/components/Timer.js
@@ -3,11 +3,22 @@ import Countdown from 'react-countdown';
 
 class Timer extends React.Component {
     render(){
+        const Completionist = () => <span>0</span>;
+        const renderer = ({ hours, minutes, seconds, completed }) => {
+            if (completed) {
+              // Render a completed state
+              console.log("Timer over!")
+              return <span>0s </span>;
+            } else {
+              // Render a countdown
+              return <span>{seconds}s </span>;
+            }
+          };
+
         return(
           <div className={"Timer"}>
-              <Countdown date={Date.now() + this.getThemeLength()}>
-                <span>Time is over!</span>
-              </Countdown>
+              <Countdown renderer={renderer} daysInHours={true} date={Date.now() + this.getThemeLength()}></Countdown>
+            | 30s
           </div>
         )
     }
@@ -15,7 +26,7 @@ class Timer extends React.Component {
     getThemeLength(){
         return(
             //Going to return the Length of the Song
-            260000
+            30000
         )
     }  
 }
