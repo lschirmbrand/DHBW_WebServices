fd63d3fefd15af196da8d5a6f3f34d94c4bab424
FabMaster2000 <68748739+FabMaster2000@users.noreply.github.com>
2021-02-15 14:20:17 +0100

    PopUp
    Can't click on other images.
    Background is changing color.

--- frontend/src/components/PopUp.js
+++ frontend/src/components/PopUp.js
@@ -15,14 +15,14 @@ class PopUp extends React.Component {
         if (this.state.display == true) {
                 return (
                     <div>
-                        <div className={"modal active"} id={"modal"}>
+                        <div className={"modal"} id={"modal"}>
                             <div className={"modal-header"}>
                                 <div className={"title"}>{this.props.answer? "True" : "False"}</div>
                                 <button onClick={() => this.closePopUp()} className={"close-button"}>&times;</button>
                             </div>
                             <div className={"modal-body"}>{this.props.answer? "True True True" : "False False False"}</div>
                         </div>
-                        <div className={"active"} id={"overlay-active"}></div>
+                        <div className={"active"} id={"overlay"}></div>
                     </div>
                 )
             }
