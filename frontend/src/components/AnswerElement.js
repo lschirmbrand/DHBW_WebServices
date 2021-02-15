e83b1d422d0918379e00144a96b1f4b103265611
FabMaster2000 <68748739+FabMaster2000@users.noreply.github.com>
2021-02-15 09:33:04 +0100

    Display PopUp
    not done

--- frontend/src/components/AnswerElement.js
+++ frontend/src/components/AnswerElement.js
@@ -1,35 +1,41 @@
 import React from "react"
-import PopUp from "./PopUp.js"
+import PopUp from "./PopUp"
+
 
 
 class AnswerElement extends React.Component{
     constructor(){
         super()
         this.state ={
-            
+            display:false
         }
     }
 
     render(props){
+        
         return (            
             <div className={"ElementCell"}>
-                    <img onClick={() => {this.checkAnswer()} } className="AnswerElement" src={this.props.object.posterURL}></img>
-                <p className="ElementPar">{this.props.object.title}</p>
+                    <img onClick={() => {this.checkAnswer()}} className={"AnswerElement"} src={this.props.object.posterURL}></img>
+                <p className={"ElementPar"}>{this.props.object.title}</p>
+                <div>{this.state.display ? <PopUp answer={this.state.answer} display={this.state.display}/> : null}</div>
             </div>
         )        
     }
 
-    checkAnswer(props){
+    checkAnswer(){
         console.log("Es wurde das Element mit der ID "+this.props.id+" geklickt.\n Richtig ist das Element mit der ID "+this.props.rightObject.id+".")
         if(this.props.id == this.props.rightObject.id){
-            <PopUp/>
-            console.log("ok")
+            this.setState({
+                display: true,
+                answer: true
+            })
         }
         else{
-            console.log("no")
+            this.setState({
+                display: true,
+                answer: false
+            })
         }
-
-        
     }
 }
 
--- frontend/src/components/PopUp.js
+++ frontend/src/components/PopUp.js
@@ -1,28 +1,42 @@
 import React from "react"
 import "../styles/PopUp.css"
 
-class PopUp extends React.Component{
 
-    render(){
-        console.log("hello")
-        return(
-            
-        <div>
-            <div className="modal" id="modal">
-                <div className="modal-header">
-                    <div className="title">Test</div>
-                    <button className="close-button">&times;</button>
-                </div>
-                <div className="modal-body">
-                    Test Test
-                </div>
-            </div>
-            <div className="active" id="overlay"></div>
-            </div>
-        )
+class PopUp extends React.Component {
 
+    constructor() {
+        super()
+        this.state = {
+            display: true
+        }
     }
 
+    render(props) {
+        if (this.state.display == true) {
+                return (
+                    <div>
+                        <div className={"modal active"} id={"modal"}>
+                            <div className={"modal-header"}>
+                                <div className={"title"}>{this.props.answer? "True" : "False"}</div>
+                                <button onClick={() => this.closePopUp()} className={"close-button"}>&times;</button>
+                            </div>
+                            <div className={"modal-body"}>{this.props.answer? "True True True" : "False False False"}</div>
+                        </div>
+                        <div className={"active"} id={"overlay-active"}></div>
+                    </div>
+                )
+            }
+        else {
+            return null;
+        }
+
+    }
+    closePopUp() {
+        console.log("Close-Button")
+        this.setState({
+            display: false
+        })
+    }
 }
 
 
