2afe08cb22a32df06342e18b42d94ee8b67fe5d6
Niel Rohling <niel.rohling@gmail.com>
2021-03-08 15:57:49 +0100

    fix spotify auth

--- backend/src/main/java/com/example/demo/dataproviders/spotify/SpotifyProvider.java
+++ backend/src/main/java/com/example/demo/dataproviders/spotify/SpotifyProvider.java
@@ -1,15 +1,9 @@
 package com.example.demo.dataproviders.spotify;
 
-import com.example.demo.Configuration;
 import com.example.demo.models.SpotifyAuth;
 import com.example.demo.models.Track;
-import org.apache.tomcat.util.http.parser.Authorization;
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.*;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,8 +12,8 @@ import java.net.URI;
 public class SpotifyProvider {
     private final RestTemplate restTemplate;
     private final SpotifyAccessTokenProvider spotifyAccessTokenProvider;
-    private final HttpEntity<Void> httpEntity;
     private final String SPOTIFY_BASE_URL = "https://api.spotify.com/v1/";
+    private HttpEntity<Void> httpEntity;
     private String access_token;
     private HttpHeaders headers;
 
@@ -32,26 +26,28 @@ public class SpotifyProvider {
         httpEntity = new HttpEntity<>(headers);
     }
 
+    private HttpEntity httpEntity(String bearerToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(bearerToken);
+        return new HttpEntity(headers);
+    }
 
 
     public Track getTrack(String id) {
         String uri = SPOTIFY_BASE_URL + "tracks/" + id;
 
-        try{
+        try {
+            HttpEntity httpEntity = httpEntity(access_token);
             ResponseEntity<Track> response = this.restTemplate.exchange(URI.create(uri), HttpMethod.GET, httpEntity, Track.class);
             return response.getBody();
-        }catch (HttpClientErrorException e){
+        } catch (HttpClientErrorException e) {
             System.out.println(" Unauthorized Exception caught!");
             SpotifyAuth spotifyAuth = spotifyAccessTokenProvider.getAuth();
             System.out.println(spotifyAuth.getAccess_token());
             access_token = spotifyAuth.getAccess_token();
-            HttpHeaders newHeaders = new HttpHeaders();
 
-            newHeaders.setContentType(MediaType.APPLICATION_JSON);
-            newHeaders.set("Authorization", "Bearer " + spotifyAuth.getAccess_token());
-            HttpEntity newHttpEntity = new HttpEntity<>(newHeaders);
-            return this.restTemplate.exchange(URI.create(uri), HttpMethod.GET, newHttpEntity, Track.class).getBody();
-
+            return getTrack(id);
         }
 
     }
