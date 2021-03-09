575dfa2e471f6aa35181761a698ceb18e113302b
Niel Rohling <niel.rohling@gmail.com>
2021-03-09 08:47:52 +0100

    added bootstrap

--- frontend/src/components/admin/Admin.js
+++ frontend/src/components/admin/Admin.js
@@ -1,38 +1,42 @@
-import React from "react"
-import "../../styles/Admin.css"
-import Movie from "./Movie"
-import Track from "./Track"
+import React from 'react';
+import '../../styles/Admin.css';
+import Movie from './Movie';
+import Track from './Track';
 
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
 class Admin extends React.Component {
     constructor() {
-        super()
+        super();
         this.state = {
             matches: [],
             loadingMovies: false,
             movies: [],
             selectedMovie: {
                 title: '',
-                id: 0
+                id: 0,
             },
             loadingTracks: false,
             tracks: [],
             selectedTrack: {
                 name: '',
-                id: ''
-            }
-        }
+                id: '',
+            },
+        };
     }
 
     componentDidMount() {
         fetch('http://localhost:8081/match')
-            .then(response => response.json())
-            .then(data => { this.setState({ matches: data }) })
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({ matches: data });
+            });
     }
 
     handleMovieInputChange = (event) => {
         const query = event.target.value;
-        this.setState({ selectedMovie: { title: query, id: 0 } })
+        this.setState({ selectedMovie: { title: query, id: 0 } });
         this.searchMovies(query);
     };
 
@@ -46,20 +50,21 @@ class Admin extends React.Component {
         const res = await fetch(url);
         const movies = await res.json();
         this.setState({ loadingMovies: false, movies: movies || [] });
-    }
+    };
 
     selectMovie = (movie) => {
         this.setState({
             selectedMovie: {
                 title: movie.title,
-                id: movie.id
-            }, movies: []
-        })
-    }
+                id: movie.id,
+            },
+            movies: [],
+        });
+    };
 
     handleTrackInputChange = (event) => {
         const query = event.target.value;
-        this.setState({ selectedTrack: { name: query, id: 0 } })
+        this.setState({ selectedTrack: { name: query, id: 0 } });
         this.searchTracks(query);
     };
 
@@ -69,44 +74,47 @@ class Admin extends React.Component {
             return;
         }
         this.setState({ loadingTracks: true });
-        const url = 'http://localhost:8081/spotify/search/' + query.replaceAll(' ', '+');
-        console.log(url)
+        const url =
+            'http://localhost:8081/spotify/search/' +
+            query.replaceAll(' ', '+');
+        console.log(url);
         const res = await fetch(url);
         const tracks = await res.json();
         this.setState({ loadingTracks: false, tracks: tracks || [] });
-    }
+    };
 
     selectTrack = (track) => {
         this.setState({
             selectedTrack: {
                 name: track.name,
-                id: track.id
-            }, tracks: []
-        })
-    }
+                id: track.id,
+            },
+            tracks: [],
+        });
+    };
 
     submit = async () => {
         console.log(this.state);
         const body = {
             tmdbID: this.state.selectedMovie.id,
-            spotifyID: this.state.selectedTrack.id
-        }
+            spotifyID: this.state.selectedTrack.id,
+        };
 
-        console.log(body)
+        console.log(body);
         const res = await fetch('http://localhost:8081/match', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(body)
-        })
+            body: JSON.stringify(body),
+        });
         const data = await res.json();
-        this.setState({ matches: [...this.state.matches, data] })
-    }
+        this.setState({ matches: [...this.state.matches, data] });
+    };
 
     render() {
         return (
             <div className="admin">
                 <div id="table">
-                    <table className="content-table">
+                    <Table striped bordered hover>
                         <thead>
                             <tr>
                                 <th>Movie</th>
@@ -114,14 +122,18 @@ class Admin extends React.Component {
                             </tr>
                         </thead>
                         <tbody>
-                            {this.state.matches.map(match => (
+                            {this.state.matches.map((match) => (
                                 <tr key={match.id}>
-                                    <td><Movie movie={match.movie} /></td>
-                                    <td><Track track={match.track} /></td>
+                                    <td>
+                                        <Movie movie={match.movie} />
+                                    </td>
+                                    <td>
+                                        <Track track={match.track} />
+                                    </td>
                                 </tr>
                             ))}
                         </tbody>
-                    </table>
+                    </Table>
                 </div>
 
                 <div className="new-form">
@@ -134,18 +146,16 @@ class Admin extends React.Component {
                             value={this.state.selectedMovie.title}
                         />
                         <div className="result-list movie-result">
-                            {this.state.movies.map(movie =>
-                                <div
-                                    className="result"
-                                    key={movie.id}
-                                >
+                            {this.state.movies.map((movie) => (
+                                <div className="result" key={movie.id}>
                                     <Movie movie={movie} />
                                     <button
-                                        onClick={e => this.selectMovie(movie)}
-                                    >✓</button>
+                                        onClick={(e) => this.selectMovie(movie)}
+                                    >
+                                        ✓
+                                    </button>
                                 </div>
-                            )}
-
+                            ))}
                         </div>
                     </div>
                     <div className="holder">
@@ -157,28 +167,29 @@ class Admin extends React.Component {
                             value={this.state.selectedTrack.name}
                         />
                         <div className="result-list track-result">
-                            {this.state.tracks.map(track =>
-                                <div
-                                    className="result"
-                                    key={track.id}
-                                >
+                            {this.state.tracks.map((track) => (
+                                <div className="result" key={track.id}>
                                     <Track track={track} />
                                     <button
-                                        onClick={e => this.selectTrack(track)}
-                                    >✓</button>
+                                        onClick={(e) => this.selectTrack(track)}
+                                    >
+                                        ✓
+                                    </button>
                                 </div>
-
-                            )}
+                            ))}
                         </div>
                     </div>
-                    <button
+                    <Button
+                        variant="primary"
                         className="submit-btn"
                         onClick={this.submit}
-                    >Submit</button>
-                </div >
-            </div >
-        )
+                    >
+                        Submit
+                    </Button>
+                </div>
+            </div>
+        );
     }
 }
 
-export default Admin
+export default Admin;
